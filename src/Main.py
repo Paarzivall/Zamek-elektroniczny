5ff39185ae3097191d18ae1294092f7b1ade3877
Paarzivall <44908654+Paarzivall@users.noreply.github.com>
2020-04-11 16:43:08 +0200

    add face recognition as second verify method

--- src/Main.py
+++ src/Main.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, Response
 from src.Option_1_Actions import Option_1_Actions
+from src.Camera import VideoCamera
 app = Flask(__name__)
+video_stream = VideoCamera()
 
 
 @app.route('/')
@@ -23,19 +25,41 @@ def option_third():
     return render_template('option3.html', title="Option Third")
 
 
-@app.route("/open", methods=['GET', 'POST'])
-def open():
+@app.route("/verify1", methods=['GET', 'POST'])
+def verify1():
     if request.method == 'POST' and request.form['option1_pin'] != '':
         pin = request.form['option1_pin']
         actions = Option_1_Actions(pin)
         if actions.is_correct() is True:
-            #tutaj wchodzi jeżeli ma prawdę tj. jeżeli taki pin jest w bazie
-            return render_template('open.html', title="Open", option="first_open")
+            # tutaj wchodzi jeżeli ma prawdę tj. jeżeli taki pin jest w bazie
+            # return render_template('option2.html', title="Option2", option="Option Second")
+            return option_second()
         else:
             return render_template('option1.html', title="Option First")
     else:
         return render_template('option1.html', title="Option First")
 
 
+def gen(camera):
+    while True:
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
+
+@app.route('/verify2')
+def video_feed():
+    return Response(gen(video_stream), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+@app.route('/verify3', methods=['GET', 'POST'])
+def check():
+    print(video_stream.get_action())
+    if video_stream.get_action() is True:
+        return option_third()
+    else:
+        return option_second()
+
+
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8888)
+    app.run(host='192.168.0.104', port=8888)
